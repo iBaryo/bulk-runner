e78a20d1243eb5f816af11783306340a8fa95510
baryo <baryo.stuff@gmail.com>
2019-07-05 14:33:30 +0300

    adding pipe/then to Pipe

--- src/utils.ts
+++ src/utils.ts
@@ -1,29 +1,49 @@
-type ArgsType<F extends Function> = F extends (...args: infer A) => unknown ? A : [];
-type ReturnVal<F extends Function> = F extends (...args: ArgsType<F>) => infer R ? R : unknown;
-type PromiseReturnVal<F extends Function> = ReturnVal<F> extends Promise<unknown> ? ReturnVal<F> : Promise<ReturnVal<F>>;
+type ArgsType<F extends Function> = F extends (...args: infer A) => unknown
+  ? A
+  : [];
+type ReturnVal<F extends Function> = F extends (...args: ArgsType<F>) => infer R
+  ? R
+  : unknown;
+type PromiseReturnVal<F extends Function> = ReturnVal<F> extends Promise<
+  unknown
+>
+  ? ReturnVal<F>
+  : Promise<ReturnVal<F>>;
 
-type FunctionAsyncThen<F extends Function> =
-  ((...args: ArgsType<F>) => PromiseReturnVal<F>)
-  & { then: <T extends Function>(fn: T) => FunctionAsyncThen<F> };
+type FunctionAsyncPipe<F extends Function> = ((
+  ...args: ArgsType<F>
+) => PromiseReturnVal<F>) & {
+  pipe: <T extends Function>(fn: T) => FunctionAsyncPipe<F>;
+  then: <T extends Function>(fn: T) => FunctionAsyncPipe<F>;
+};
 
+export function createPipe<T extends Function>(
+  pipeSrc: T
+): FunctionAsyncPipe<T> {
+  return createFunctionsPipe([pipeSrc]);
+}
 
-export function createPipe<T extends Function>(pipeSrc: T): FunctionAsyncThen<T> {
-  const fns = [pipeSrc] as Function[];
-
-  async function pipe(...args: any[]) { // async reduce
-    let res = args;
-    for (let currFn of fns) {
-      res = await currFn(...(res || args));
-    }
-    return res;
-  }
-
-  return Object.assign(pipe, {
-    then<N extends Function>(this: FunctionAsyncThen<T>, nextFn: N) {
-      fns.push(nextFn);
-      return this;
+function createFunctionsPipe<T extends Function>(
+  pipeFns: Function[]
+): FunctionAsyncPipe<T> {
+  return (Object.assign(
+    async function asyncReduce(...args: any[]) {
+      let res = args;
+      for (let currFn of pipeFns) {
+        res = await currFn(...(res || args));
+      }
+      return res;
     },
-  }) as Function as FunctionAsyncThen<T>;
+    {
+      pipe<N extends Function>(this: FunctionAsyncPipe<T>, nextFn: N) {
+        pipeFns.push(nextFn);
+        return this;
+      },
+      then<N extends Function>(this: FunctionAsyncPipe<T>, nextFn: N) {
+        return createFunctionsPipe(pipeFns.concat(nextFn));
+      },
+    }
+  ) as Function) as FunctionAsyncPipe<T>;
 }
 
 export function logBulk<T>(bulk: T[], bulkIndex: number) {
--- test/utils.test.ts
+++ test/utils.test.ts
@@ -5,17 +5,43 @@ describe('utils', () => {
   const mockIndex = 0;
   const mockResult = 'done';
 
+  it('should pipe through functions', async () => {
+    let num = 0;
+    const assertAndAdvance = (x: number) => {
+      expect(x).toBe(num);
+      return ++num;
+    };
+
+    const pipe = createPipe(assertAndAdvance);
+
+    const thenNum = 10;
+    for (let i = 1; i < thenNum; i++) {
+      pipe.pipe(assertAndAdvance);
+    }
+
+    await pipe(num);
+
+    expect(num).toBe(thenNum);
+  });
+
   it('should pipe through logging the bulk', async () => {
     jest.spyOn(console, 'log');
 
-    const pipe = createPipe(logBulk).then((bulk: string[], bulkIndex: number) => {
-      expect(console.log).toHaveBeenCalledWith(`bulk index: ${bulkIndex} (${bulk.length} entries)`);
-      expect(bulk).toBe(mockBulk);
-      expect(bulkIndex).toBe(mockIndex);
-      return mockResult;
-    });
+    const pipe = createPipe(logBulk).then(
+      (bulk: string[], bulkIndex: number) => {
+        expect(console.log).toHaveBeenCalledWith(
+          `bulk index: ${bulkIndex} (${bulk.length} entries)`
+        );
+        expect(bulk).toBe(mockBulk);
+        expect(bulkIndex).toBe(mockIndex);
+        return mockResult;
+      }
+    );
 
-    const res = await pipe(mockBulk, mockIndex);
+    const res = await pipe(
+      mockBulk,
+      mockIndex
+    );
     expect(res).toBe(mockResult);
   });
 });
