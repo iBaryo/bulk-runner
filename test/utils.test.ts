8c5a4679b5d8b662a6b84e39e71c59d4f10e7da6
baryo <or.ba@gigya-inc.com>
2019-07-12 14:46:21 +0300

    updating keywords

--- test/utils.test.ts
+++ test/utils.test.ts
@@ -27,16 +27,14 @@ describe('utils', () => {
   it('should pipe through logging the bulk', async () => {
     jest.spyOn(console, 'log');
 
-    const pipe = createPipe(logBulk).then(
-      (bulk: string[], bulkIndex: number) => {
-        expect(console.log).toHaveBeenCalledWith(
-          `bulk index: ${bulkIndex} (${bulk.length} entries)`
-        );
-        expect(bulk).toBe(mockBulk);
-        expect(bulkIndex).toBe(mockIndex);
-        return mockResult;
-      }
-    );
+    const pipe = createPipe(logBulk).then((bulk, bulkIndex: number) => {
+      expect(console.log).toHaveBeenCalledWith(
+        `bulk index: ${bulkIndex} (${bulk.length} entries)`
+      );
+      expect(bulk).toBe(mockBulk);
+      expect(bulkIndex).toBe(mockIndex);
+      return mockResult;
+    });
 
     const res = await pipe(
       mockBulk,
